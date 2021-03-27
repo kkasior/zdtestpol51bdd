8a1f0a996d09ac2002d0b7e1ade1c3e41d5bcb70
kkasior <karol.kasior@gmail.com>
2021-03-27 14:20:48 +0100

    change Open first seeing blog implementation to return another page object

--- src/test/java/zdtestpol51bdd/devto/pages/MainPage.java
+++ src/test/java/zdtestpol51bdd/devto/pages/MainPage.java
@@ -22,8 +22,9 @@ public class MainPage {
         PageFactory.initElements(this.driver, this);
     }
 
-    public void selectFirstBlog(){
+    public SingleBlogPage selectFirstBlog(){
         firstBlog.click();
+        return new SingleBlogPage(this.driver);
     }
 
     public void goToPodcastSection(){
